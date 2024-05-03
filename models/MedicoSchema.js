f61b577419622354492178db6b0f03a2d287f563
giovanninms <jovincu123@gmail.com>
2024-05-03 17:19:12 -0300

    aaa

--- models/MedicoSchema.js
+++ models/MedicoSchema.js
@@ -5,42 +5,53 @@ const especializacoes = Object.freeze({
     Joelho: "Joelho",
     Quadril: "Quadril",
     Ombro: "Ombro",
-    Tornozelo: "Tornozelo", 
+    Tornozelo: "Tornozelo",
     Trauma: "Trauma"
 })
 const medicoSchema = new schema({
-    nome:{
+    nome: {
         type: String,
         required: true
-    }, 
-    cpf:{
-        type: String, 
-        required: true
-    }, 
-    crm:{
+    },
+    cpf: {
         type: String,
         required: true
-    }, 
-    dataNascimento:{
+    },
+    crm: {
+        type: String,
+        required: true
+    },
+    dataNascimento: {
         type: Date,
         required: true
-    }, 
-    especializacao:{
+    },
+    especializacao: {
         type: String,
         enum: Object.values(especializacoes)
-  },
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    telone: {
+        type: String
+    },
+    celular: {
+        type: String,
+        required: true
+    },
     createdAt: {
-        type: Date, 
+        type: Date,
         default: () => Date.now(),
         immutable: true
-    }, 
-    updateAt:{
+    },
+    updateAt: {
         type: Date,
         default: Date.now()
     }
 })
 
-Object.assign(medicoSchema.statics,{
+Object.assign(medicoSchema.statics, {
     especializacoes
 })
 
