792aaca839c5a4f037f80e6ee3e6d5462001b46f
giovanninms <jovincu123@gmail.com>
2024-05-14 17:57:03 -0300

    Implementação das telas: cadastro, edição do hoapital

--- routers/hospital.js
+++ routers/hospital.js
@@ -13,58 +13,116 @@ router.post("/filtro", (req, res) => {
     const selectFiltro = req.body.selectFiltro
 
     if (selectFiltro === "nomeFantasia") {
-        HospitalSchema.findOne({nomeFantasia: inputFiltro}).lean().then((hospital) => {
+        HospitalSchema.findOne({ nomeFantasia: inputFiltro }).lean().then((hospital) => {
             console.log(`Hospital ${hospital.nomeFantasia},`)
-            res.render("hospital/filtro", {hospital: hospital})
+            res.render("hospital/filtro", { hospital: hospital })
         }).catch((error) => {
             console.log("Erro ao consultar o hospital pelo nome fantasia: " + error)
         })
-    }else if(selectFiltro === "cnpj"){
-        HospitalSchema.findOne({cnpj: inputFiltro}).lean().then((hospital) => {
+    } else if (selectFiltro === "cnpj") {
+        HospitalSchema.findOne({ cnpj: inputFiltro }).lean().then((hospital) => {
             console.log(`Hospital ${hospital.nomeFantasia},`)
-            res.render("hospital/filtro", {hospital: hospital})
+            res.render("hospital/filtro", { hospital: hospital })
         }).catch((error) => {
             console.log("Erro ao consultar o hospital pelo cnpj: " + error)
         })
-    }else if(selectFiltro === "todos"){
+    } else if (selectFiltro === "todos") {
         HospitalSchema.find().lean().then((hospitais) => {
             hospitais.forEach(hospital => {
                 console.log(`Hospital: ${hospital.nomeFantasia}`)
-    
+
             });
             res.render("hospital/index", { hospitais: hospitais })
         }).catch((error) => {
             console.log("Error ao consultar todos os hospitais: " + error)
         })
     }
-    
+
 })
 
+router.get("/novoHospital", (req, res) => {
+    res.render("hospital/novoHospital")
+})
+
+router.post("/novoHospital/add", (req, res) => {
+    const { razaoSocial, nomeFantasia, cnpj, email, telefone, celular, rua, bairro, cidade, cep } = req.body
+
+    const erros = []
+    if (!razaoSocial || typeof razaoSocial === undefined || razaoSocial === null) {
+        erros.push({ menssagem: "Razão Social inválida! Tente novamente." })
+    }
+    if (!nomeFantasia || typeof nomeFantasia === undefined || nomeFantasia === null) {
+        erros.push({ menssagem: "Nome Fantasia inválido! Tente novamente." })
+    }
+    if (!cnpj || typeof cnpj === undefined || cnpj === null || cnpj.length < 18) {
+        erros.push({ menssagem: "CNPJ inválido! Tente novamente." })
+    }
+    if (!telefone || typeof telefone === undefined || telefone === null || telefone.length < 14) {
+        erros.push({ menssagem: "Telefone inválido! Tente novamente." })
+    }
+    if (!celular || typeof celular === undefined || celular === null || celular.length < 15) {
+        erros.push({ menssagem: "Celular inválido! Tente novamente." })
+    }
+    if (!rua || typeof rua === undefined || rua === null) {
+        erros.push({ menssagem: "Rua inválida! Tente novamente." })
+    }
+    if (!bairro || typeof bairro === undefined || bairro === null) {
+        erros.push({ menssagem: "Bairro inválido! Tente novamente." })
+    }
+    if (!cidade || typeof cidade === undefined || cidade === null) {
+        erros.push({ menssagem: "Cidade inválida! Tente novamente." })
+    }
+    if (!cep || typeof cep === undefined || cep === null) {
+        erros.push({ menssagem: "CEP inválido! Tente novamente." })
+    }
+
+    if (erros.length > 0) {
+        res.render("hospital/novoHospital", { erros: erros })
+    } else {
+        const novoHospital = ({
+            razaoSocial: razaoSocial,
+            nomeFantasia: nomeFantasia,
+            cnpj: cnpj,
+            email: email,
+            telefone: telefone,
+            celular: celular,
+            endereco: [{
+                rua: rua,
+                bairro: bairro,
+                cidade: cidade,
+                cep: cep
+            }]
+        })
+        new HospitalSchema(novoHospital).save().then(() => {
+            console.log("Hospital cadastrado com sucesso!")
+            req.flash("msg_sucesso", "Hospital cadastrado com sucesso!")
+            res.render("hospital/index")
+        }).catch((error) => {
+            console.log("Erro ao cadastrar hospital: " + error)
+            req.flash("msg_erro", "Erro ao cadastrar Hospital")
+            res.render("hospital/novoHospital")
+        })
+    }   
+})
+
+router.get("/detalheHospital/:id", (req, res)=>{
+    HospitalSchema.findOne({ _id: req.params.id }).lean().then((hospital) => {
+        console.log(`Consulta Hospital pelo _id, ${hospital.endereco[0].rua}`)
+        res.render("hospital/detalheHospital", {hospital: hospital, endereco: hospital.endereco[0]})
+    }).catch((error) => {
+        console.log("Erro ao consultar o hospital pelo _id: " + error)
+    })    
+})
+router.get("/editarHospital/:id", (req, res)=>{
+    HospitalSchema.findOne({ _id: req.params.id }).lean().then((hospital) => {
+        console.log(`Consulta Hospital pelo _id, ${hospital.endereco[0].rua}`)
+        res.render("hospital/editarHospital", {hospital: hospital, endereco: hospital.endereco[0]})
+    }).catch((error) => {
+        console.log("Erro ao consultar o hospital pelo _id: " + error)
+    })
+})
 // //CRUD do hospital
-//     //Create
-// const novoHospital = ({
-//     razaoSocial: "UNIORT.E - ORTOPEDIA ESPECIALIZADA LTDA",
-//     nomeFantasia: "CENTRO DE ORTOPEDIA UNIORTE",
-//     cnpj: "10.246.214/0001-04",
-//     email: "uniorte@email.com",
-//     telefone: "(43) 3377-0900",
-//     celular: "(43) 93377-0900",
-//     endereco: [{
-//         rua: "AVENIDA HIGIENOPOLIS, 2600",
-//         bairro: "PARQUE GUANABARA",
-//         cidade: "Londrina",
-//         cep: "86.050-000"
-//     }]
-// })
-// new HospitalSchema(novoHospital).save().then(() => {
-//     console.log("Hospital cadastrado com sucesso!")
-//     //req.flash("msg_sucesso", "Hospital cadastrado com sucesso!")
-//     //res.render("index")
-// }).catch((error) => {
-//     console.log("Erro ao cadastrar hospital: " + error)
-//     //req.flash("msg_erro", "Erro ao cadastrar Hospital")
-//     //res.render("index")
-// })
+//     
 //     //Read
 //         
 //         //consulta de todos os hospitais
